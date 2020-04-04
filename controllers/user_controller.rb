7fef3be906760cfc986be4d232218b8e6abe78bc
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-04-04 11:29:02 +0100

    can set monthly limit

---
+++ controllers/user_controller.rb
@@ -0,0 +1,21 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require('pry-byebug')
+require_relative('../models/user.rb')
+also_reload('../models/*')
+
+# USER INDEX
+
+get '/user' do
+  @users = User.all
+  erb(:"user/index")
+end
+
+# USER UPDATE MONTHLY LIMIT
+
+post '/user/limit' do
+  @user = User.find_by_id(params['id'].to_i)
+  @user.monthly_limit = params['monthly_limit'].to_i
+  @user.update
+  erb(:"user/limit")
+end
